e2730b30c89fe3312e25a9bf837a434d7f08f2e8
Harsh18894 <harshdeep18894@gmail.com>
2020-04-24 03:16:16 +0530

    Initial Commit

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+import Navbar from './Navbar'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <div className="container-fluid mt-5">
+          <div className="row">
+            <main role="main" className="col-lg-12 d-flex text-center">
+              <div className="content mr-auto ml-auto">
+
+              </div>
+            </main>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/contracts/Election.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.0;
+
+
+contract Election {
+    // Store Candidate
+    // Read Candidate
+    // Constructor
+    string public candidate;
+
+    constructor() public {
+        candidate = "Johnathan Doe";
+    }
+}
